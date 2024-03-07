530012689581d308acb067783de5839e069b515f
Ravikanth <ravikanththummala91@gmail.com>
2024-03-07 18:10:27 -0500

    Added Stat of reminder

---
+++ RemindersApp/Views/ReminderStatView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderStatView.swift
+//  RemindersApp
+//
+//  Created by Ravikanth  on 3/7/24.
+//
+
+import SwiftUI
+
+struct ReminderStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderStatView()
+}
